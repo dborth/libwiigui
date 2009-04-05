f734ceb15d3c80e328d36201685ede17f6dbc9ba
dborth <dborth@19eb663e-200b-11de-b650-e715bd6d7cf1>
2009-04-05 03:16:10 +0000

    change default sound format to 16bit PCM 48000, add loop paramter to sound

--- source/libwiigui/gui.h
+++ source/libwiigui/gui.h
@@ -117,15 +117,22 @@ class GuiSound
 		void Pause();
 		//!Resume sound playback
 		void Resume();
+		//!Checks if the sound is currently playing
+		//!\return true if sound is playing, false otherwise
+		bool IsPlaying();
 		//!Set sound volume
 		//!\param v Sound volume (0-100)
 		void SetVolume(int v);
+		//!Set the sound to loop playback (only applies to OGG)
+		//!\param l Loop (true to loop)
+		void SetLoop(bool l);
 	protected:
 		const u8 * sound; //!< Pointer to the sound data
 		int type; //!< Sound format type (SOUND_PCM or SOUND_OGG)
 		s32 length; //!< Length of sound data
 		s32 voice; //!< Currently assigned ASND voice channel
 		s32 volume; //!< Sound volume (0-100)
+		bool loop; //!< Loop sound playback
 };
 
 //!Primary Gui class
