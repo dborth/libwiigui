138a150b79276dfe955105af476aba770f1cf210
dborth <dborth@19eb663e-200b-11de-b650-e715bd6d7cf1>
2009-04-13 21:27:50 +0000

    [1.00 - April 4, 2009] * Initial release

---
+++ tags/1.00/source/menu.h
@@ -0,0 +1,25 @@
+/****************************************************************************
+ * libwiigui Template
+ * Tantric 2009
+ *
+ * menu.h
+ * Menu flow routines - handles all menu logic
+ ***************************************************************************/
+
+#ifndef _MENU_H_
+#define _MENU_H_
+
+#include <ogcsys.h>
+
+void InitGUIThreads();
+void MainMenu (int menuitem);
+
+enum
+{
+	MENU_EXIT = -1,
+	MENU_NONE,
+	MENU_SETTINGS,
+	MENU_SETTINGS_FILE
+};
+
+#endif
