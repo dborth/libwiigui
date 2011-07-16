07f4b0836c40b065db021a60a1b0b691de30053e
dborth <dborth@19eb663e-200b-11de-b650-e715bd6d7cf1>
2011-07-16 23:33:59 +0000

    update for dkppc r24

--- source/filebrowser.h
+++ source/filebrowser.h
@@ -26,8 +26,6 @@ typedef struct
 
 typedef struct
 {
-	u64 offset; // DVD offset
-	unsigned int length; // file length
 	char isdir; // 0 - file, 1 - directory
 	char filename[MAXJOLIET + 1]; // full filename
 	char displayname[MAXDISPLAY + 1]; // name for browser display
